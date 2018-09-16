4c14cd6c260ae4dfd29ba6e675769ef1bd66b0b0
Hamza Hedfi <h.hedfi@outlook.com>
2018-09-16 13:06:56 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,12 @@
 def turn_count(board)
   count = 0
   board.each do |cell|
-    if cell == "X" or cell == "O" 
+    if cell == "X" or cell == "O"
       count += 1
     end
   end
   return count
+end
+
+def current_player(board)
 end
